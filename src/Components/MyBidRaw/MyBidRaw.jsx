d80539085cf0b06d826e7537e6e3afb629884836
ikmat-rabib <ikmat.rbb@gmail.com>
2023-12-05 12:46:46 +0600

    fixed my bid

--- src/Components/MyBidRaw/MyBidRaw.jsx
+++ src/Components/MyBidRaw/MyBidRaw.jsx
@@ -1,29 +1,33 @@
 
 
-const MyBidRaw = ({bid}) => {
+const MyBidRaw = ({ bid }) => {
 
-    const {_id, job_title, employerEmail, deadline, status} = bid
+    const { _id, job_title, employerEmail, deadline, status } = bid
+
+    const handleCompleteBid = _id => {
+        console.log(_id);
+    }
 
     return (
-            <tr key={_id}>
-                                <th>1</th>
-                                <td>{job_title}</td>
-                                <td>{employerEmail
-                                }</td>
-                                <td>{deadline
-                                }</td>
-                                <td >{status}</td>
-                                <td >
-                                    {status === 'Pending' && (
-                                        <button
-                                            onClick={() => handleCompleteBid(bid._id)}
-                                            className="btn bg-[#5bbb7b] hover:bg-[#43a062] text-indigo-800 border-0"
-                                        >
-                                            Complete
-                                        </button>
-                                    )}
-                                </td>
-                            </tr>
+        <tr key={_id}>
+            <th></th>
+            <td>{job_title}</td>
+            <td>{employerEmail
+            }</td>
+            <td>{deadline
+            }</td>
+            <td >{status}</td>
+            <td >
+                {status === 'Pending' && (
+                    <button
+                        onClick={() => handleCompleteBid(_id)}
+                        className="btn bg-[#5bbb7b] hover:bg-[#43a062] text-indigo-800 border-0"
+                    >
+                        Complete
+                    </button>
+                )}
+            </td>
+        </tr>
     );
 };
 
